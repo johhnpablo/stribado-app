a118065c68ec453d63f48ba13edd68adfaf09606
John Pablo <92765140+johhnpablo@users.noreply.github.com>
2024-11-22 08:48:18 -0300

    create app and create login page

---
+++ pages/auth/login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen bg-slate-900">
+    <div class="bg-slate-800 w-3/12 p-6 rounded-lg h-min-60 shadow-sm">
+      <form-login />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import formLogin from "./-components/form-login.vue";
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtPage />
+  <div>
+    teste
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
